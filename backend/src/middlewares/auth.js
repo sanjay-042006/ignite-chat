30df73bbd13e5161812332910ea99a8da5c69c13
sanjay-042006 <sanjaygopisanjay26@gmail.com>
2026-03-29 22:13:52 +0530

    apk updated

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -3,7 +3,11 @@ import prisma from '../lib/db.js';
 
 export const protectRoute = async (req, res, next) => {
     try {
-        const token = req.cookies.jwt;
+        let token = req.cookies.jwt;
+
+        if (!token && req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+            token = req.headers.authorization.split(' ')[1];
+        }
 
         if (!token) {
             return res.status(401).json({ message: 'Unauthorized - Token missing' });
